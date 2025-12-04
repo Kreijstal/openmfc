128381a78fbf3f4cf0c5ad971126f8dfc0a0c859
Kreijstal <rainb@tfwno.gf>
2025-12-04 19:42:04 +0100

    Fix MSVC compatibility in exceptions.c
    Use __declspec(dllimport) and __stdcall for MSVC,
    __attribute__ syntax for GCC/MinGW

--- src/mfc/exceptions.c
+++ src/mfc/exceptions.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 
 // Import _CxxThrowException from vcruntime140.dll
-// Note: On MinGW, this might be named differently or require specific declaration.
-// For x64, it's usually available.
-#ifdef _WIN32
-#define STDCALL __attribute__((stdcall))
+#ifdef _MSC_VER
+    // MSVC syntax
+    __declspec(dllimport) void __stdcall _CxxThrowException(void* pExceptionObject, const ThrowInfo* pThrowInfo);
 #else
-#define STDCALL
+    // GCC/MinGW syntax
+    __attribute__((dllimport)) void __attribute__((stdcall)) _CxxThrowException(void* pExceptionObject, const ThrowInfo* pThrowInfo);
 #endif
-
-__attribute__((dllimport)) void STDCALL _CxxThrowException(void* pExceptionObject, const ThrowInfo* pThrowInfo);
 
 // External helpers implemented in C++ (afxmem.cpp)
 void* OpenMFC_New_CMemoryException(void);
@@ -29,8 +27,11 @@ void AfxThrowFileException(int cause, int lOsError, const char* pFileName) {
 }
 
 // Thunks for copy constructors (referenced by RTTI)
-// For now, these are just stubs. In a real implementation, they would call the C++ copy ctor.
-void STDCALL CException_CopyCtor(void* dest, void* src) {
+#ifdef _MSC_VER
+void __stdcall CException_CopyCtor(void* dest, void* src) {
+#else
+void __attribute__((stdcall)) CException_CopyCtor(void* dest, void* src) {
+#endif
     // TODO: Implement copy logic if needed.
     // For simple exceptions, shallow copy might be enough, or we might need to call the actual copy ctor.
     // Since we don't have the C++ class definition here, we can't easily do it.
