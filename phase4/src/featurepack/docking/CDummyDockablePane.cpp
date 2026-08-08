1c2177a2359a2b29a97bb1c065661dacb7481a2b
Kreijstal <rainb@tfwno.gf>
2026-08-08 10:32:02 +0200

    Fix CDummyDockablePane msgmap stubs

--- phase4/src/featurepack/docking/CDummyDockablePane.cpp
+++ phase4/src/featurepack/docking/CDummyDockablePane.cpp
@@ -2,14 +2,15 @@
 // Sources: manual_small_stub_implementations.cpp
 
 #include "detail/ManualSmallStubImplementationsSupport.h"
+#include "detail/Pane15MsgmapSupport.h"
 
 // Symbol: ?GetMessageMap@CDummyDockablePane@@MEBAPEBUAFX_MSGMAP@@XZ
 extern "C" void* MS_ABI impl__GetMessageMap_CDummyDockablePane__MEBAPEBUAFX_MSGMAP__XZ(
     void* pThis) {
     (void)pThis;
-    return nullptr;
+    return &classCDummyDockablePane_msgmap;
 }
 // Symbol: ?GetThisMessageMap@CDummyDockablePane@@KAPEBUAFX_MSGMAP@@XZ
 extern "C" void* MS_ABI impl__GetThisMessageMap_CDummyDockablePane__KAPEBUAFX_MSGMAP__XZ() {
-    return nullptr;
+    return &classCDummyDockablePane_msgmap;
 }
